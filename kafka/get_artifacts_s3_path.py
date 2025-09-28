d02920518b460f8684cfda47901ed1ec790c740d
Safarveisi <sajad.safarveisi@gmail.com>
2025-09-28 16:59:02 +0200

    Refactored

--- kafka/get_artifacts_s3_path.py
+++ kafka/get_artifacts_s3_path.py
@@ -14,23 +14,25 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import boto3
 
 # Will cause an error if one of the following env variables does not exist
-S3_REGION = os.environ["S3_REGION"]
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-S3_ENDPOINT_URL = os.environ["S3_ENDPOINT_URL"]
-S3_BUCKET = os.environ["S3_BUCKET"]
+S3_REGION_NAME = os.environ["S3_REGION_NAME"]
+S3_ACCESS_KEY = os.environ["S3_ACCESS_KEY"]
+S3_SECRET_KEY = os.environ["S3_SECRET_KEY"]
+S3_HOST = os.environ["S3_HOST"]
+S3_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
 
 ROOT_PREFIX = (
-    "mlflow_dev/"  # Example hard-coded root prefix where experiments are stored
+    "self_service_mlops/"  # Example hard-coded root prefix where experiments are stored
 )
 MAX_WORKERS = 4
 
 session = boto3.session.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=S3_REGION,
+    aws_access_key_id=S3_ACCESS_KEY,
+    aws_secret_access_key=S3_SECRET_KEY,
+    region_name=S3_REGION_NAME,
 )
-s3 = session.client("s3", endpoint_url=S3_ENDPOINT_URL)
+s3 = session.client(
+    "s3", endpoint_url=S3_HOST if S3_HOST.startswith("http") else f"http://{S3_HOST}"
+)
 
 
 def list_experiment_prefixes():
@@ -40,7 +42,9 @@ def list_experiment_prefixes():
     """
     prefixes = []
     paginator = s3.get_paginator("list_objects_v2")
-    for page in paginator.paginate(Bucket=S3_BUCKET, Prefix=ROOT_PREFIX, Delimiter="/"):
+    for page in paginator.paginate(
+        Bucket=S3_BUCKET_NAME, Prefix=ROOT_PREFIX, Delimiter="/"
+    ):
         for cp in page.get("CommonPrefixes", []):
             p = cp.get("Prefix")
             if p:
@@ -56,7 +60,7 @@ def scan_prefix_for_substr(
     Short-circuits if stop_event is set, or returns the first matching key.
     """
     paginator = s3.get_paginator("list_objects_v2")
-    for page in paginator.paginate(Bucket=S3_BUCKET, Prefix=prefix):
+    for page in paginator.paginate(Bucket=S3_BUCKET_NAME, Prefix=prefix):
         if stop_event.is_set():
             return None
         for obj in page.get("Contents", []):
@@ -81,7 +85,7 @@ def find_first_match() -> str | None:
             if key:
                 stop_event.set()
                 # Optionally: cancel others (not strictly necessary; they'll see stop_event or finish soon)
-                return f"s3://{S3_BUCKET}/{key}"
+                return f"s3://{S3_BUCKET_NAME}/{key}"
     return None
 
 
