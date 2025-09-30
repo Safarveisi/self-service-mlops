12ce502e0b8cb5bc21a2ac4c0e83e7add370b7c4
Safarveisi <sajad.safarveisi@gmail.com>
2025-09-30 15:48:21 +0200

    Added notifier deployment for successful endpoint creation

---
+++ kafka/notify_endpoint_creation.py
@@ -0,0 +1,56 @@
+import os
+from utils import get_logger
+import json
+from kafka import KafkaConsumer
+
+
+log = get_logger()
+
+
+required = {
+    "KAFKA_CLIENT_PASSWORDS": os.getenv("KAFKA_CLIENT_PASSWORDS", ""),
+    "KAFKA_TOPIC_MODEL_PRODUCTION_ENDPOINT": os.getenv(
+        "KAFKA_TOPIC_MODEL_PRODUCTION_ENDPOINT", ""
+    ),
+    "KAFKA_SASL_USERNAME": os.getenv("KAFKA_SASL_USERNAME", ""),
+    "KAFKA_BOOTSTRAP": os.getenv("KAFKA_BOOTSTRAP", ""),
+}
+
+missing = [k for k, v in required.items() if not v]
+
+if missing:
+    raise RuntimeError(f"Missing required env vars: {', '.join(missing)}")
+
+kafka_args = {
+    "bootstrap_servers": required["KAFKA_BOOTSTRAP"],
+    "group_id": "my_group",
+    "value_deserializer": lambda v: json.loads(v.decode("utf-8")),
+    "auto_offset_reset": "earliest",
+    "enable_auto_commit": False,
+    "security_protocol": "SASL_PLAINTEXT",
+    "sasl_mechanism": "SCRAM-SHA-256",
+    "sasl_plain_username": required["KAFKA_SASL_USERNAME"],
+    "sasl_plain_password": required["KAFKA_CLIENT_PASSWORDS"],
+}
+
+consumer = KafkaConsumer(
+    required["KAFKA_TOPIC_MODEL_PRODUCTION_ENDPOINT"],
+    **kafka_args,
+)
+
+# We can extend the logic here
+for message in consumer:
+    inference_service_name = message.value.get("inference_service_name", "")
+    inference_service_namespace = message.value.get("inference_service_namespace", "")
+    experiment_id = message.value.get("experiment_id", "")
+    run_id = message.value.get("run_id", "")
+    status = message.value.get("status", "")
+    message = message.value.get("message", "")
+
+    log.info(
+        f"Status for {inference_service_name} in namespace {inference_service_namespace} is {status}."
+        f" Experiment ID: {experiment_id}, Run ID: {run_id}."
+    )
+    log.info(f"{message}")
+
+    consumer.commit()  # Manually commit the offset after processing the message
