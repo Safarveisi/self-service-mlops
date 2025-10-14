e7ef522461884bf67eb16de81e9416160dfd691e
Safarveisi <sajad.safarveisi@gmail.com>
2025-10-14 13:51:53 +0200

    Removed unnecessary comments

--- terraform/k8s/modules/providers.tf
+++ terraform/k8s/modules/providers.tf
@@ -21,15 +21,6 @@ terraform {
 }
 
 provider "ionoscloud" {
-#  we encourage users to use token authentication for security reasons
-#  username          = var.ionos_username
-#  password          = var.ionos_password
+# Use token authentication for security reasons
   token             = var.ionos_token
-#  optional, to be used only for reseller accounts
-#  contract_number = "contract_number_here"
-#  optional, does not need to be configured in most cases
-#  endpoint = "custom_cloud_api_url"
-#  s3_access_key     =  "your_access_key"
-#  s3_secret_key     =  "your_secret_key"
-
 }
